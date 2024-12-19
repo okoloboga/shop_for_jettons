44025a80388fbd1f7c61b9f39f77a877374162ca
user <okoloboga37@gamil.com>
2024-12-19 14:37:15 +0300

    Changed colors and box placement

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ const {
 
 const app = express();
 const cache = apicache.middleware;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cache('2 minutes'))
 app.use('/api', router);
